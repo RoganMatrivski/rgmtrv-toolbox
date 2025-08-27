68f6ab6cb6f89f93e433a51701c38dd1a85d6337
Robin Mauritz <robinmauritzrm@gmail.com>
2025-08-27 15:42:58 +0000

    chore: Dummy push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<div className="flex items-center justify-center min-h-screen">
-			<span className="text-4xl">Hi, welcome. Now go away.</span>
+			<span className="text-4xl">Hi, welcome. Now go away. </span>
 		</div>
 	);
 }
